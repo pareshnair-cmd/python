9d0a3dc3d39b9e4c7219474f77af454b74f195fc
pareshnair-cmd <5yu9>
2026-02-04 16:11:39 +0530

    hello

---
+++ While loop (Trip to the gas station).py
@@ -0,0 +1,5 @@
+n = int(input("Locating nearest gas station :"))
+while n >= 2:
+    n -= 1
+    print (n)
+print ("You have arrived")
--- While loop.py
+++
@@ -1,5 +0,0 @@
-n = int(input("you enter"))
-while n <= 100:
-    n += 1
-    print (n)
-print ("greeting")
