76176f58654c968d06dd93e7b1448d64ef3cd420
pareshnair-cmd <5yu9>
2026-02-06 16:44:09 +0530

    hello

--- Sqeldon.py
+++
@@ -1,8 +0,0 @@
-import turtle
-turtle.Screen().bgcolor("white")
-turtle.Screen().setup(400,400)
-square = turtle.Turtle()
-for i in range(4):
-    square.forward(100)
-    square.left(90)
-turtle.done()
