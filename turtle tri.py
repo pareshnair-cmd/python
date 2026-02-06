810b1f7247d78b57e32bef9a854b5954ed8f258a
pareshnair-cmd <5yu9>
2026-02-06 16:45:13 +0530

    hello

---
+++ turtle tri.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.Screen().bgcolor("white")
+turtle.Screen().setup(400,400)
+triangle = turtle.Turtle()
+for i in range(3):
+   triangle.forward(100)
+   triangle.left(120)
+turtle.done()
