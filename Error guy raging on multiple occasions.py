10759a496f9fe19ee3dc37c49b6c635693d21f51
pareshnair-cmd <5yu9>
2026-02-22 12:06:16 +0530

    hello

---
+++ Error guy raging on multiple occasions.py
@@ -0,0 +1,13 @@
+try:
+    num1,num2 = eval(input("enter two numbers seprated by commas:"))
+    result = num1 / num2
+    print (result)
+    print ("Took you long enough dummy \ _ / ")
+except ZeroDivisionError:
+    print ("If you have been IN SCHOOL AND FOCUSED IN MATHS YOU WOULD HAVE NOT DIVIDED ZERO BY ANY. OTHER. NUMBER!! ")
+except SyntaxError:
+    print ("I just want to ask you ONE question. DID YOU READ THE 2ND LINE!!!!")
+except NameError:
+    print ("LITTLE PIECE OF POO! IT TOLD 'NUM1 - NUM2' ARE YOU THAT DUM >:( ")
+if num1 or num2 == 67:
+    print ("#$@* ##@*$ its 2026 ##@*$ DX")
