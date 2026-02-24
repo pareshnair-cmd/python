40730922f61678fc4deb241a825097132815249e
pareshnair-cmd <5yu9>
2026-02-24 16:10:40 +0530

    Homework

---
+++ Age check (more like checks for errors!).py
@@ -0,0 +1,8 @@
+try:
+    age = int(input("enter age:"))
+    print ("your age is")
+    print (age)
+except ValueError:
+    print (":(     Sorry,an error has occoured please try- Uh. -ah Correcting the ValueError? - by computer error manager")
+finally:
+    print (":(     Python error feeder has gone on vacation - by computer error manager")
