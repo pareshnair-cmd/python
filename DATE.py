0c864cf45fb7605abb2cdec038ad91d6b129e7e1
pareshnair-cmd <5yu9>
2026-03-01 11:37:45 +0530

    hello

---
+++ DATE.py
@@ -0,0 +1,11 @@
+import datetime
+DATE = (datetime.datetime.now())
+print (DATE)
+from datetime import date,time,datetime
+print (date.today())
+import calendar
+print (calendar.month(2026,3))
+for i in range (13):
+    print (calendar.month_name[i])
+for i in range (8):
+    print ("\n",calendar.day_name[i])
