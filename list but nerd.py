60eaca40cfe529541877f18c4b8e9173ca7caa06
pareshnair-cmd <5yu9>
2026-03-08 11:33:22 +0530

    hello

---
+++ list but nerd.py
@@ -0,0 +1,11 @@
+listmath = [46,78,91,190,89,123]
+sum = 0
+for i in listmath:
+    sum = sum + i
+final_sum = sum / 6
+print (listmath)
+print (final_sum) 
+print (max(listmath))  
+print (min(listmath))  
+sor = listmath.sort()
+print(sor)
