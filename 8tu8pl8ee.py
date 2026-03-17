d801ce03857582a505a8ed6bc2c94f06a64b8c6b
pareshnair-cmd <5yu9>
2026-03-17 16:01:41 +0530

    hello

---
+++ 8tu8pl8ee.py
@@ -0,0 +1,4 @@
+tuple45 = (90,2,89,45,79,35)
+product = (tuple45[0])*(tuple45[1])*(tuple45[2])*(tuple45[3])*(tuple45[4])*(tuple45[5])
+print (tuple45)
+print ("tuple45 : multiplying the items =",product)
