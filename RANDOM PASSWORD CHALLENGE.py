08f7e82b0a2f1af42ac88498eb1c86ffcce0992e
pareshnair-cmd <5yu9>
2026-04-13 17:01:52 +0530

    hello

---
+++ RANDOM PASSWORD CHALLENGE.py
@@ -0,0 +1,6 @@
+import random
+list1 = ['a','s','q','m','u','w','x','d','g','f','c','h','k']
+list2 = [1,2,3,4,5,6,7,8,9,0]
+for i in range(5):
+    print(random.choice(list1))
+    print(random.choice(list2))
