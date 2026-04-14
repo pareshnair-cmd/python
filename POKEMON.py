50533b6c26b90805e433ee84adbce6c9d3a0c9b8
pareshnair-cmd <5yu9>
2026-04-14 09:07:17 +0530

    hello

--- POKEMON.py
+++ POKEMON.py
@@ -11,8 +11,16 @@ print (pokemon3.species)
 userchoice = str(input("Choose your pokemon: "))
 if userchoice == "Bulbasuar" or userchoice == "bulbasuar":
     print(pokemon1.species,",",pokemon1.type,"type")
+    userchoice = str(input("Are you sure:  "))
+    if userchoice == 'yes':
+        print("You just got a",pokemon1.species)
 elif userchoice == "Charizard" or userchoice == "charizard":
-    print(pokemon2.species,",",pokemon2.type,"type")    
+    print(pokemon2.species,",",pokemon2.type,"type")
+    userchoice = str(input("Are you sure:  "))
+    if userchoice == 'yes':
+        print("You just got a",pokemon2.species)    
 elif userchoice == "Sqiurtle" or userchoice == "sqiurtle":
     print(pokemon3.species,",",pokemon3.type,"type")
-
+    userchoice = str(input("Are you sure: "))
+    if userchoice == 'yes':
+        print("You just got a",pokemon3.species)
--- RANDOM PASSWORD CHALLENGE.py
+++ RANDOM PASSWORD CHALLENGE.py
@@ -1,6 +1,9 @@
 import random
 list1 = ['a','s','q','m','u','w','x','d','g','f','c','h','k']
 list2 = [1,2,3,4,5,6,7,8,9,0]
-for i in range(5):
-    print(random.choice(list1))
-    print(random.choice(list2))
+list3 = ['@','#','*','%','>']
+for i in range(4):
+  print(random.choice(list1))
+  print(random.choice(list2))
+  print(random.choice(list3))
+
